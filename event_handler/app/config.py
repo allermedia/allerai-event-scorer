e266f8d0b9158a0d7c468aed9d5f73f3714c46da
Maltehestrup <Malte.hastrup@aller.com>
2025-08-07 15:46:30 +0200

    test run

--- event_handler/app/config.py
+++ event_handler/app/config.py
@@ -1,4 +1,7 @@
 import os
 
-PROJECT_ID = os.getenv("TARGET_PROJECT_ID")
-OUTPUT_TOPIC = os.getenv("OUTPUT_TOPIC")
+#PROJECT_ID = os.getenv("TARGET_PROJECT_ID")
+#OUTPUT_TOPIC = os.getenv("OUTPUT_TOPIC")
+
+PROJECT_ID = "aller-content-tool"
+OUTPUT_TOPIC = "allerai-scorer-events-push"
