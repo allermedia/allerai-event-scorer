956715a080acd88ea5411af4eaf33b3d246efa65
Maltehestrup <Malte.hastrup@aller.com>
2025-08-08 15:01:35 +0200

    scorer first version

--- event_scorer/app/pubsub.py
+++ event_scorer/app/pubsub.py
@@ -6,7 +6,6 @@ class PubSubService:
         self.publisher = pubsub_v1.PublisherClient()
         self.topic_path = self.publisher.topic_path(project_id, output_topic)
 
-    def publish(self, message_dict: dict):
-        attributes = {"event_source": "ai-platform"}
+    def publish(self, message_dict: dict, attributes: dict):
         message_bytes = json.dumps(message_dict).encode('utf-8')
         self.publisher.publish(self.topic_path, message_bytes, **(attributes or {}))
