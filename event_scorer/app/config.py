c5d28429fc5f7a12aa7286f4e9c45c7b99b72122
Maltehestrup <Malte.hastrup@aller.com>
2025-08-11 15:23:22 +0200

    error log tables and topics and subs

--- event_scorer/app/config.py
+++ event_scorer/app/config.py
@@ -4,3 +4,4 @@ credentials, project_id = google.auth.default()
 
 PROJECT_ID = project_id
 OUTPUT_TOPIC = "allerai-scorer-events-push"
+OUTPUT_TOPIC_ERROR_LOG = "allerai-scorer-events-push-error-log"
