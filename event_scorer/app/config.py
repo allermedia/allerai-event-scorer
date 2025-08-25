01cff5eb75f0e982574ca8a3bc574d17d2563271
Maltehestrup <Malte.hastrup@aller.com>
2025-08-25 13:55:39 +0200

    refactoring

--- event_scorer/app/config.py
+++ event_scorer/app/config.py
@@ -1,7 +1,9 @@
 import google.auth
+import os
 
 credentials, project_id = google.auth.default()
 
 PROJECT_ID = project_id
 OUTPUT_TOPIC = "allerai-scorer-events-push"
 OUTPUT_TOPIC_ERROR_LOG = "allerai-scorer-events-push-error-log"
+ADP_PROJECT_ID = os.getenv("TARGET_PROJECT_ID")
